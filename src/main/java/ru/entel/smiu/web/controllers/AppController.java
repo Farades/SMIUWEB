f817003abdd2336f5f6a86fc87a751d877c8f572
farades <creg2007@gmail.com>
2015-10-29 14:22:52 +0300

    add left menu

--- src/main/java/ru/entel/smiu/web/controllers/AppController.java
+++ src/main/java/ru/entel/smiu/web/controllers/AppController.java
@@ -4,4 +4,6 @@ package ru.entel.smiu.web.controllers;
  * Created by farades on 29.09.15.
  */
 public class AppController {
+    public final static String FIRMWARE_VERSION = "0.5";
+    public final static String HARDWARE_VERSION = "0.3";
 }
